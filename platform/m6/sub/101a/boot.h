a1b5b92ba0ce659cf01cc616eab2d1eea4b1cb33
Cedric Bompart <cedric@agileape.co.uk>
2022-07-25 11:53:48 +0100

    Initialised the file modules

--- platform/m6/sub/101a/boot.h
+++ platform/m6/sub/101a/boot.h
@@ -4,5 +4,7 @@
 extern void _set_canon_mmu_tables_to(unsigned addr);
 extern void _make_copy_of_canon_mmutables(unsigned dest_addr);
 extern int _CreateTask(char *name, int prio, int stack_size, void *entry, int parm);
+extern int _PostLogicalEventToUI(int event, int unk);
+extern void task_InitFileModules(void);
 
 #endif
